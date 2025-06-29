96933da57a10b9eae709759c4960552427d0c48f
Owen Erhabor <erhaborowen3@gmail.com>
2025-06-29 10:47:56 +0100

    Enhance SnakeFood class to include canvas element and color properties; update script.js to initialize SnakeFood with canvas and implement random placement logic.

--- SnakeFood.js
+++ SnakeFood.js
@@ -1,8 +1,43 @@
 "use strict";
-import { GameAsset } from "./GameAsset.js";
+import { GameAsset, Drawer } from "./GameAsset.js";
 class SnakeFood extends GameAsset {
-  constructor() {
+  constructor({
+    canvasElement,
+    snakeFoodColor,
+    snakeFoodBorderColor,
+    initialPlacement,
+  } = {}) {
     super();
+    this._element = canvasElement;
+    this.snakeFoodColor =
+      snakeFoodColor ||
+      getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-food-fill")
+        .trim();
+    this.snakeFoodBorderColor =
+      snakeFoodBorderColor ||
+      getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-food-stroke")
+        .trim();
+    this._placement = initialPlacement || { x: 9, y: 7 };
+  }
+  randomPlacement() {
+    const gridPerRowOrColumn = this._element?.width / this.gridSize || 15;
+    this._placement.x = Math.floor(Math.random() * gridPerRowOrColumn);
+    this._placement.y = Math.floor(Math.random() * gridPerRowOrColumn);
+  }
+  reAlignPlacement(gridOffset) {
+    this._placement.x += gridOffset;
+    this._placement.y += gridOffset;
+  }
+  paint(context) {
+    Drawer.drawRect({
+      context,
+      x: this._placement.x * this.gridSize,
+      y: this._placement.y * this.gridSize,
+      strokeStyle: this.snakeFoodBorderColor,
+      fillStyle: this.snakeFoodColor,
+    });
   }
 }
 
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import { Snake } from "./Snake.js";
 import { SnakeFood } from "./SnakeFood.js";
 
 function initializeGame() {
+  // creates all necessary variables
   const canvasElement = document.querySelector("#game-canvas");
   const gameCanvas = new GameCanvas({ canvasElement });
   /** @type {CanvasRenderingContext2D} */
@@ -11,25 +12,36 @@ function initializeGame() {
   const initialGridX = Math.floor(gameCanvas.width / gameCanvas.gridSize / 2);
   const initialGridY = Math.floor(gameCanvas.height / gameCanvas.gridSize / 2);
   const snake = new Snake({ snakeHead: { x: initialGridX, y: initialGridY } });
-  const snakeFood = new SnakeFood();
+  const snakeFood = new SnakeFood({ canvasElement });
   handleScreenResize();
+
   function paintGameAssests(context) {
     gameCanvas.paint();
     snake.paint(context);
+    snakeFood.paint(context);
   }
+
   function alignGameAssets() {
     let sizes = gameCanvas.resize();
     /* the current size - previous size / sides to account for gridX 1 + gridY 1 = 2 */
     let pixelOffset = (sizes[1] - sizes[0]) / 2;
-    let gridOffset = pixelOffset / snake.gridSize;
+    let gridOffset = pixelOffset / gameCanvas.gridSize;
     snake.reAlignBodyParts(gridOffset);
+    snakeFood.reAlignPlacement(gridOffset);
   }
+
+  function handleSnakeEatFood() {
+    // adding more later
+    snakeFood.randomPlacement();
+    paintGameAssests(context);
+  }
+
   function handleScreenResize() {
     alignGameAssets();
     paintGameAssests(context);
-    console.log(snake.bodyParts);
   }
 
+  // add eventlisteners
   window.addEventListener("resize", handleScreenResize);
 }
 
